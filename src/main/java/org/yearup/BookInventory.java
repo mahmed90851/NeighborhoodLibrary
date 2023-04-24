bc42cce9d28280c34171c041ffa9f9e336939508
MohammedAhmed90851 <130098921+mahmed90851@users.noreply.github.com>
2023-04-24 15:59:33 -0400

    updated code

--- src/main/java/org/yearup/BookInventory.java
+++ src/main/java/org/yearup/BookInventory.java
@@ -2,7 +2,7 @@ package org.yearup;
 
 public class BookInventory {
     static Book[] booksAvailable = {
-            new Book(12345, "85378754","Book of Lambhorgini" , false, ""),
+            new Book(12345, "85378754","Book of Lamborghini" , false, ""),
             new Book(12346, "56787987","Book of Ferrari" , false, ""),
             new Book(12347, "86087676", "Book of Nissan", false, ""),
             new Book(12348, "09876843","Book of Toyota" , false, ""),
